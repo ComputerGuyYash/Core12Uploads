04017afead9e73ccc4a7c47d427f45c0105a86d1
Computer-CGuy <computer.pythonlearner@gmail.com>
2019-10-12 00:58:30 +0530

    Cool Changes

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ $(window).scroll(function () {
   var height = innerHeight;
   wScroll=$(this).scrollTop();
   x = wScroll/2;
+  $('.bft').css('transform','translateY('+(((wScroll-($('.bft').offset().top))/1.3))+'px)')
+  $('.doraemaan').css('transform','translateY('+(((wScroll-($('.doraemaan').offset().top))/2))+'px)')
+  $('.CC').css('transform','translateY('+(((wScroll-($('.CC').offset().top))/1.3)+50)+'px)')
   $('.navbar').css('background-color','rgba(255,255,255,'+wScroll/(height*2)+')');
   $('.navbar').css('border-color','rgba(0,0,0,'+wScroll/(height*2)+')');
   $('.slider').css('transform','rotate(-30deg) translateX('+(600-x)+'px) translateY(-100px)')
-  if(wScroll > $('.slide3').offset().top){
+  if(wScroll > ($('.slide3').offset().top)-200){
     console.log(((wScroll-$('.slide3').offset().top)/parseInt($('.slide3').css('height').replace("px",""))*100))
     $('.side1').css('width',(((wScroll-$('.slide3').offset().top)/parseInt($('.slide3').css('height').replace("px",""))*100)+50)+'%')
     $('.side2').css('width',((100-(((wScroll-$('.slide3').offset().top)/parseInt($('.slide3').css('height').replace("px",""))*100)))-51)+'%')
